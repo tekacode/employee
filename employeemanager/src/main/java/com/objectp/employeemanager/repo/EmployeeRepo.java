5ae3e1791cf356decad0b5a73587a2a49e57f684
tekacode <81430335+tekacode@users.noreply.github.com>
2021-06-12 18:21:06 -0400

    first commit

---
+++ employeemanager/src/main/java/com/objectp/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,14 @@
+package com.objectp.employeemanager.repo;
+
+import com.objectp.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+   Optional<Employee> findEmployeeById(Long id);
+
+    void deleteEmployeeById(Long id);
+
+}
